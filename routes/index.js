340846281650498c4d6bb75819c48be71adb72f5
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-09-04 08:27:06 -0700

    task 3 + 4

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  // App Controller
+
+  // should return if redis is alive and if DB is alive
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  // should return the number of users and files in DB
+  router.get('/stats', (req,res) => {
+    AppController.getStats(req, res);
+  });
+
+  // User Controller
+
+  // should create a new user in DB
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  // should retrieve the user base on the token used
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+}
+
+export default controllerRouting;
