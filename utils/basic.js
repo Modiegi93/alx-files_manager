0de55a4ea15dccf7f704f70d69b59b4f4cd1240f
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-09-06 01:23:50 -0700

    compulsory tasks

--- utils/basic.js
+++ utils/basic.js
@@ -1,6 +1,14 @@
 import { ObjectId } from 'mongodb';
 
+/**
+ * Module with basic utilities
+ */
 const basicUtils = {
+  /**
+   * Checks if Id is Valid for Mongo
+   * @id {string|number} id to be evaluated
+   * @return {boolean} true if valid, false if not
+   */
   isValid(id) {
     try {
       ObjectId(id);
