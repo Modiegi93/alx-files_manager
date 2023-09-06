c183e0a008afec35392d3eb840c13c5cf54301e8
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-09-06 00:04:47 -0700

    task 1&2

--- utils/db.js
+++ utils/db.js
@@ -21,33 +21,33 @@ class DBClient {
 	this.db = false;
       }
     });
-  }
+   }
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
-  isAlive() {
-    return Boolean(this.db);
-  }
+   /**
+    * Checks if connection to Redis is Alive
+    * @return {boolean} true if connection alive or false if not
+    */
+   isAlive() {
+     return Boolean(this.db);
+   }
 
-  /**
-   * Returns the number of documents  in the collection users
-   * @return {number} amount of users
-   */
-  async nbUsers() {
-    const numberOfUsers = this.usersCollection.countDocuments();
-    return numberOfUsers;
-  }
+   /**
+    * Returns the number of documents  in the collection users
+    * @return {number} amount of users
+    */
+   async nbUsers() {
+     const numberOfUsers = this.usersCollection.countDocuments();
+     return numberOfUsers;
+   }
 
-  /**
-   * Returns the number of documents in the collection files
-   * @return {number} amount of files
-   */
-  async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
-    return numberOfFiles;
-  }
+   /**
+    * Returns the number of documents in the collection files
+    * @return {number} amount of files
+    */
+   async nbFiles() {
+     const numberOfFiles = this.filesCollection.countDocuments();
+     return numberOfFiles;
+   }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -16,45 +16,45 @@ class RedisClient {
     this.client.on('connect', () => {
       // console.log('Redis client connected to the server');
     });
-  }
+   }
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
-  isAlive() {
-    return this.client.connected;
-  }
+   /**
+    * Checks if connection to Redis is Alive
+    * @return {boolean} true if connection alive or false if not
+    */
+   isAlive() {
+     return this.client.connected;
+   }
 
-  /**
-   * gets value corresponding to key in redis
-   * @key {string} key to search for in redis
-   * @return {string} value of key
-   */
-  async get(key) {
-    const value = await this.getAsync(key);
-    return value;
-  }
+   /**
+    * gets value corresponding to key in redis
+    * @key {string} key to search for in redis
+    * @return {string} value of key
+    */
+   async get(key) {
+     const value = await this.getAsync(key);
+     return value;
+   }
 
-  /**
-   * Creates a new key in redis with a specific TTL
-   * @key {string} key to be saved in redis
-   * @value {string} value to be assigned to key
-   * @duration {number} TTL of key
-   * @return {undefined} No return
-   */
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
-  }
+   /**
+    * Creates a new key in redis with a specific TTL
+    * @key {string} key to be saved in redis
+    * @value {string} value to be assigned to key
+    * @duration {number} TTL of key
+    * @return {undefined} No return
+    */
+   async set(key, value, duration) {
+     this.client.setex(key, duration, value);
+   }
 
-  /**
-   * Deletes key in redis service
-   * @key {string} key to be deleted
-   * @return {undefined} No return
-   */
-  async del(key) {
-    this.client.del(key);
-  }
+   /**
+    * Deletes key in redis service
+    * @key {string} key to be deleted
+    * @return {undefined} No return
+    */
+   async del(key) {
+     this.client.del(key);
+   }
 }
 
 const redisClient = new RedisClient();
